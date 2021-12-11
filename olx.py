9e720f5c622fbe45ec3467f8570db0a7da2157bd
ARAVIND CHANDRASHEKAR REDDY <achandrashekarr@deakin.edu.au>
2021-12-11 10:02:32 +0530

    refined the code

--- olx.py
+++ olx.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0'}
+headers = {'User-Agent': 'Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83)'}
 final = ''    #results are appended to this string.
 
 #======================For loop to get results based on price==============
 
-for i,y in zip([40000,50000,60000,70000],[50000,60000,70000,80000]):
+#for i,y in zip([0000,30000],[30000,40000]):
+for i,y in zip([45000,50000,60000],[50000,60000,70000]):
     html =   requests.get(f'https://www.olx.in/hosur_g4059174/q-iphone-12?filter=price_between_{i}_to_{y}&isSearchCall=true&sorting=asc-price',headers=headers)
     soup = BeautifulSoup(html.content,'html.parser')
     li_items = soup.find_all('li',{'data-aut-id':'itemBox'}) 
